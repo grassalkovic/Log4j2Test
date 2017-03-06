6ba38da629245b71328ab0e6518c37880a3858a9
grassalkovic <grassalkovic@gmail.com>
2017-03-06 14:44:55 +0100

    blabal

--- src/Log4j2Test.java
+++ src/Log4j2Test.java
@@ -1,7 +1,5 @@
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.ThreadContext;
 
 import static org.apache.logging.log4j.Level.ERROR;
 
